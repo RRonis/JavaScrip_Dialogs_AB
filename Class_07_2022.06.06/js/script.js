e602127f07daa9f6fa14e136f66f3bf88e3085c2
Normunds H <>
2022-06-07 10:55:27 +0300

    šahs

---
+++ Class_07_2022.06.06/js/script.js
@@ -0,0 +1,8 @@
+
+console.log(435||0)
+
+document.getElementById("demo").innerHTML = 435||0 ;
+   
+    // function myFunction() {
+    //     document.getElementById("demo").innerHTML = "Paragraph changed.";
+    //     }
