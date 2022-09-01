56b38634ab0d594c89cb4301803ad686833d1132
Normunds H <>
2022-09-01 09:02:28 +0300

    pēc nodarbības

--- Class_27_2022.08.31_EVENTS/js/script.js
+++ Class_27_2022.08.31_EVENTS/js/script.js
@@ -1,32 +1,100 @@
-// Piemēra - https://www.javascripttutorial.net/javascript-dom/javascript-getelementsbyclassname/ - HTML dokumenta analīze:
+// https://www.javascripttutorial.net/javascript-dom/javascript-events/
 
-let btn = document.getElementById("btnRate");
+let btn = document.querySelector('#btn');
 
-btn.addEventListener("click", () => {
-  let btn_group1 = document.getElementById("btn-group1");
-  let btns1 = btn_group1.getElementsByClassName("rate1");
+// variants #1
+// function display() {
+//     alert('It was clicked!');
+// }
+// btn.addEventListener('click',display);
+//----------------------------------------
 
-/*  let data = [];
-  for (let i = 0; i < btns1.length; i++) {
-    console.log([btns1[i].value, btns1[i].checked]);
-    data.push([btns1[i].value, btns1[i].checked]);
-    //console.log(data);
-  }*/
+// variants #2
+// btn.addEventListener('click',function() {
+//     alert('It was clicked! ');
+// });
+// -----------------------------------------
 
-/*  let data = [];
-  for (let btn of btns1) {
-    console.log([btn.value, btn.checked]);
-    data.push([btn.value, btn.checked]);
-  }*/
-
-  let data = [].map.call(btns1, btn => [btn.value, btn.checked]);
-
-  console.log(data);
-  /*    
-    let btn_group2 = document.getElementById('btn-group2');
-    let btns2 = btn_group2.getElementsByClassName('rate2');
-    
-    data = [].map.call(btns2, btn => [btn.value, btn.checked]);
-    console.log(data);
-    */
+// variants #3
+btn.addEventListener('click', (event) => {
+  // alert('It was clicked!');
+  console.log("Event type: " + event.type);
+  console.log("Event bubbles?: " + event.bubbles);
+  console.log("Cancelable?: " + event.cancelable);
+  console.log("currentTarget?: " + event.currentTarget);
+  console.log("       target?: " + event.target);
+  // event.currentTarget.innerText = 'Ouch!';
+  console.log("defaultPrevented?: " + event.defaultPrevented);
+  console.log("More infor about event: " + event.detail);
+  console.log("eventPhase: " + event.eventPhase);
+  event.preventDefault();
+  console.log("default prevented pec 'event.preventDefault()': " + event.defaultPrevented);
+  console.log("--------------------------------------------");
 });
+
+// ======================= PIEMERS 2 =====================================================
+// https://developer.mozilla.org/en-US/docs/Web/API/Event/currentTarget
+// piemērs kurā tiek paslēpts elements uz kura tiek uzklikšķināts + arī paslēpjam body (ar laika nobīdi 2 sek):
+
+function hide(e){
+  let currentTarget_ = e.currentTarget;
+  let target_ = e.target;
+  //console.log(currentTarget_.tagName);
+  if (currentTarget_.tagName == 'P')
+      setTimeout(() => {currentTarget_.style.visibility = 'hidden';}, 1000);
+      //setTimeout(() => {target_.style.visibility = 'hidden';}, 1000);
+  else
+      setTimeout(() => {currentTarget_.style.visibility = 'hidden';}, 2000);
+      //setTimeout(() => {target_.style.visibility = 'hidden';}, 2000);
+  e.stopPropagation();
+  console.log("e.currentTarget: ",e.currentTarget);
+  console.log("e.target: ",e.target);
+  // When this function is used as an event handler: this === e.currentTarget
+}
+
+const ps = document.getElementsByTagName('p');
+
+// izveidojam tik EventListenerus cik ir paragrāfu:
+for (let i = 0; i < ps.length; i++){
+  // console: print the clicked <p> element
+  ps[i].addEventListener('click', hide, false);
+}
+
+// Ja aktivizē arī šo tad tiek paslēpti visi 'body' elementi:
+document.body.addEventListener('click', hide, false);
+
+// testējam preventDfault (atverot linku)
+let link = document.querySelector('a');
+link.addEventListener('click',function(event) {
+    console.log('clicked');
+    event.preventDefault();
+    console.log(event.currentTarget.href);
+    let jsWindow = window.open(
+        event.currentTarget.href,
+        //"https://www.w3schools.com/js/js_object_prototypes.asp",
+        "test",
+        "height=600,width=800"
+      );
+});
+
+// ==================== PIEMERS 2 BEIGAS =================================
+
+  
+  // Click around and make paragraphs disappear
+
+
+  // When this function is used as an event handler: this === e.currentTarget
+
+
+
+// btn.addEventListener('mouseup', (event) => { // mousedown mouseover
+//   console.log("Event type: " + event.type);
+//   console.log("Event bubbles?: " + event.bubbles);
+//   console.log("Cancelable?: " + event.cancelable);
+//   console.log("currentTarget?: " + event.currentTarget);
+//   // šis zemāk nomaina pogas tekstu
+//   event. currentTarget.innerText= event.detail + "-klikskis";
+//   console.log("defaultPrevented?: " + event.defaultPrevented);
+//   console.log("More infor about event: " + event.detail);
+//   console.log("======");
+// });
