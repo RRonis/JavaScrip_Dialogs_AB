34ff69458daefd145cfb5728c062ad4543f1a1e5
Normunds H <>
2022-10-03 17:58:12 +0300

    pirms 03-Okt.nodarbības

---
+++ Class_38_IU_Uzdevums/js/2iuscript.js
@@ -0,0 +1,67 @@
+// https://www.telerik.com/blogs/save-for-later-feature-in-forms-using-localstorage
+
+const formId = "pasutijuma-forma"; // formas ID no html
+const url = location.href; //  lapas href priekš Local storage
+const formIdentifier = `${url} ${formId}`; // Identificējam formu
+const saveButton = document.querySelector("#save"); // select save button
+const alertBox = document.querySelector(".alert"); // select alert display div
+let form = document.querySelector(`#${formId}`); // select form
+let formElements = form.elements; // get the elements in the form
+
+/**
+ * This function gets the values in the form
+ * and returns them as an object with the
+ * [formIdentifier] as the object key
+ * @returns {Object}
+ */
+const getFormData = () => {
+  let data = { [formIdentifier]: {} };
+  for (const element of formElements) {
+    if (element.name.length > 0) {
+      data[formIdentifier][element.name] = element.value;
+    }
+  }
+  return data;
+};
+
+saveButton.onclick = event => {
+  event.preventDefault();
+  data = getFormData();
+  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+  const message = "Formas dati saglabāti!";
+  displayAlert(message);
+};
+
+/**
+ * This function displays a message
+ * on the page for 1 second
+ *
+ * @param {String} message
+ */
+const displayAlert = message => {
+  alertBox.innerText = message;
+  alertBox.style.display = "block";
+  setTimeout(function() {
+    alertBox.style.display = "none";
+  }, 2000);
+};
+
+/**
+ * This function populates the form
+ * with data from localStorage
+ *
+ */
+const populateForm = () => {
+  if (localStorage.key(formIdentifier)) {
+    const savedData = JSON.parse(localStorage.getItem(formIdentifier)); // get and parse the saved data from localStorage
+    for (const element of formElements) {
+      if (element.name in savedData) {
+        element.value = savedData[element.name];
+      }
+    }
+    const message = "Form has been refilled with saved data!";
+    displayAlert(message);
+  }
+};
+
+document.onload = populateForm(); // populate the form when the document is loaded
--- Class_38_IU_Uzdevums/js/iuscript.js
+++ Class_38_IU_Uzdevums/js/iuscript.js
@@ -1,52 +1,68 @@
-const produkti = ["Asorti", "Bekona", "Garneļu tīģeris","Kebaba","Prošuto"];
-const productSelect = document.getElementById('product');
 
+//   https://dev.to/michaelburrows/how-to-save-data-in-localstorage-using-javascript-994
 
-// Izlasām local storage
+// Produktu katalogs (kods, apraksts)
+const produkti = new Array("Asorti", "Bekona", "Garneļu tīģeris", "Kebaba", "Prošuto");
 
-// Ierakstam local storage saturu html
+    const pr0 = produkti[0];
+    document.getElementById("pr0").innerHTML = pr0;
+    const pr1 = produkti[1];
+    document.getElementById("pr1").innerHTML = pr1;
+    const pr2 = produkti[2];
+    document.getElementById("pr2").innerHTML = pr2;
+    const pr3 = produkti[3];
+    document.getElementById("pr3").innerHTML = pr3;
+    const pr4 = produkti[4];
+    document.getElementById("pr4").innerHTML = pr4;
 
-// FUNKCIJA pievienot pasūtijumu
+// html elementi ar kurem strādāsim 
 
-// btn.addEventListener('click',pievienot);
-function pievienot() {
-    localStorage.setItem("lastname", "Smith");
-}
+const noteForm = document.getElementById("pas-forma");
+const noteInput = document.getElementById("produkts");
+const noteSubmit = document.getElementById("ord-submit");
+const notes = document.getElementById("notes");
 
+// ielādējam esošos pasūtījumus no localStorage
 
-// FUNKCIJA izpidlīt pasūtījumu
+let notesStorage = localStorage.getItem("notes")
+  ? JSON.parse(localStorage.getItem("notes")) : [];
+    
+  var count = {}; 
+    notesStorage.forEach(element => {
+        count[element] = (count[element] || 0) + 1;
+    });
 
+// funkcija, kas saglabā jauno pasūtījumu pēc formas aizpildīšanas un submit   
 
+noteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  notesStorage.push(noteInput.value);
+  localStorage.setItem("notes", JSON.stringify(notesStorage));
+  listBuilder(noteInput.value);
+  noteInput.value = "";
+});
 
+// ar listbuilderi izveidojam sarakstu ar pasūtītājiem produktiem 
+// no localStorage un nosūtām uz html
 
-window.onload=function(){
-  var minusBtn = document.getElementById("minus"),
-      plusBtn = document.getElementById("plus"),
-      numberPlace = document.getElementById("numberPlace"),
-      submitBtn = document.getElementById("submit"),
-      number = 1, /// num vērtība
-      min = 0, /// min limits
-      max = 30; /// max limits
+const listBuilder = (text) => {
+  const note = document.createElement("li");
+  note.innerHTML = text + '   <button onclick="deleteNote(this)">Pasūtījums Izpildīts</button>';
+  notes.appendChild(note);
+};
 
-  // Saglabājam pasūtījumu prod+skaits local storage
-  function SaveOrderData(productIndex, productPrice) {
-      localStorage.setItem('selectedProductIndex', productIndex);
-      localStorage.setItem('selectedProductPrice', productPrice);
-  }
-  const selectedProductIndex = localStorage.getItem('selectedProductIndex');
-      
- 
-    izmest.onclick = function(){
-        number = 0; /// nonullējam
-        numberPlace.innerText = number ; /// aizsūtam 0 uz html
-        alert("Vai vēlies atgriezties pie Piedāvājuma?");
-        } 
-         
-  }
-  submitBtn.onclick = function(){
-    alert("Izvēlēts " + "Produkta ID " + "k-ktu skaits: " + number);
-   }
-  
+const getNotes = JSON.parse(localStorage.getItem("notes"));
+getNotes.forEach((note) => {
+  listBuilder(note);
+});
 
+// izvēlētajam list elementam caur btn tiek nodots indekss pēc kura tad arī 
+// izdzēš konkrēto localStorage ierakstu
 
-
+const deleteNote = (btn) => {
+  let el = btn.parentNode;
+  const index = [...el.parentElement.children].indexOf(el);
+  notesStorage.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesStorage));
+  el.remove();
+};
